de3119fc36d29b836baeb322ccb1580354418b05
hemu789 <87188130+hemu789@users.noreply.github.com>
2023-08-23 17:28:21 +0530

    bfs and dfs

--- graph.cpp
+++ graph.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <queue> 
 #include <vector>
 // harsh Dsa
 
 using namespace std;
+
+void bfs(vector<vector<int>> v, int sv)
+{
+
+    int n = v.size();
+
+    vector<bool> visited(n, false);
+    queue<int> q;
+    q.push(sv);
+    visited[sv] = true;
+
+    while (!q.empty())
+    {
+
+        int cv = q.front();
+        q.pop();
+        cout << cv << endl;
+        // adj vertex of cv(cuure vertex)
+        for (int i = 0; i < n; i++)
+        {
+            if (v[cv][i] == 1 && visited[i] == false)
+            {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+}
 
 void print(vector<vector<int>> v, int sv, vector<bool> &visited)
 {
@@ -36,11 +65,15 @@ int main()
         matrix[sv][fv] = 1;
     }
 
-    cout << "Printing" << endl;
+    cout << "DFS Printing" << endl;
 
     vector<bool> visited(n, false);
 
     print(matrix, 0, visited);
+
+    cout << "BFS Printing" << endl;
+
+    bfs(matrix, 0);
 
     return 0;
 }
